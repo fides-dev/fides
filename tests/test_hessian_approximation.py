a06dc3009cd3750c8d3e628863f845d3fc874bd4
FFroehlich <fabian@schaluck.com>
2020-11-15 14:40:00 -0500

    add tests for hessian approximation dimension checks

---
+++ tests/test_hessian_approximation.py
@@ -0,0 +1,22 @@
+from fides import BFGS
+
+import pytest
+import numpy as np
+
+
+def test_wrong_dim():
+    with pytest.raises(ValueError):
+        BFGS(hess_init=1)
+
+    with pytest.raises(ValueError):
+        BFGS(hess_init=np.ones(2,))
+
+    with pytest.raises(ValueError):
+        BFGS(hess_init=np.ones(2, 3, 2))
+
+    with pytest.raises(ValueError):
+        BFGS(hess_init=np.ones(2, 3))
+
+    with pytest.raises(ValueError):
+        h = BFGS(hess_init=np.ones(2, 2))
+        h.init_mat(3)
